7e2fb5d887cfde99c3306f97280bdf7189703574
Jason Takahashi <jmtakahashi@gmail.com>
2026-03-16 07:11:00 -0700

    undo previous commit.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,11 @@ const cookieParser = require('cookie-parser');
 const corsOptions = require('./config/corsOptions');
 const { NotFoundError } = require('./utils/expressError');
 
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const todoRoutes = require('./routes/todos');
+const debugRoutes = require('./routes/debug');
+
 const app = express();
 
 app.use(cors(corsOptions));
@@ -17,11 +22,6 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/users');
-const todoRoutes = require('./routes/todos');
-const debugRoutes = require('./routes/debug');
 
 // router routes
 app.use('/auth', authRoutes);
