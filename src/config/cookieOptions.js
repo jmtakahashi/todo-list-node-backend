54fe5523ed852c34f49c6f561f360ccc776a0c24
Jason Takahashi <jmtakahashi@gmail.com>
2026-03-16 06:42:41 -0700

    update domain property on cookieOptions.

--- src/config/cookieOptions.js
+++ src/config/cookieOptions.js
@@ -1,9 +1,10 @@
 const { path } = require("../app");
 
 const cookieOptions = {
+  domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : 'localhost',
   httpOnly: true,
   secure: process.env.NODE_ENV === 'test' ? false : true,
-  sameSite: 'Lax', // Allow cross-site cookies (required for secure cookies)
+  sameSite: 'lax', // Allow cross-site cookies (required for secure cookies)
   // maxAge: 24 * 60 * 60 * 1000, // 1 day
   maxAge: 7 * 24 * 60 * 60 * 1000, // same as the refresh token expiry time (7 days)
   path: '/auth/refresh', // only send the refresh token cookie to the /auth/refresh endpoint
