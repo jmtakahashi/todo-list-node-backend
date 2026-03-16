3eb8b7a167b254b06f0c07d0b2f5c26ee9f0b976
Jason Takahashi <jmtakahashi@gmail.com>
2026-03-16 06:31:17 -0700

    update sameSite option to be 'lax'

--- src/config/cookieOptions.js
+++ src/config/cookieOptions.js
@@ -3,7 +3,7 @@ const { path } = require("../app");
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'test' ? false : true,
-  sameSite: 'None', // Allow cross-site cookies (required for secure cookies)
+  sameSite: 'Lax', // Allow cross-site cookies (required for secure cookies)
   // maxAge: 24 * 60 * 60 * 1000, // 1 day
   maxAge: 7 * 24 * 60 * 60 * 1000, // same as the refresh token expiry time (7 days)
   path: '/auth/refresh', // only send the refresh token cookie to the /auth/refresh endpoint
