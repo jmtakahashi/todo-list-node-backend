56a1230b285bfb664b24a6040968affc647ba55b
Jason Takahashi <jmtakahashi@gmail.com>
2026-03-15 21:07:38 -0700

    change cookie expiry to match token expiry of 7 days.

--- src/config/cookieOptions.js
+++ src/config/cookieOptions.js
@@ -1,9 +1,9 @@
 const cookieOptions = {
   httpOnly: true,
-  secure: true,
+  secure: process.env.NODE_ENV === 'test' ? false : true,
   sameSite: 'None', // Allow cross-site cookies (required for secure cookies)
   // maxAge: 24 * 60 * 60 * 1000, // 1 day
-  maxAge: 60 * 60 * 24 // same as the refresh token expiry time so that the cookie will be automatically cleared when the refresh token expires
+  maxAge: 7 * 24 * 60 * 60 * 1000, // same as the refresh token expiry time (7 days)
 };
 
 module.exports = cookieOptions;
