263791104b3999c6c4b89d6f2c5da90a0bd6a561
Jason Takahashi <jmtakahashi@gmail.com>
2026-03-16 23:28:47 -0700

    add origin header to console.log.

--- src/middleware/debug.js
+++ src/middleware/debug.js
@@ -3,6 +3,7 @@ const debug = (req, res, next) => {
   console.log('Request Method:'.brightCyan, req.method);
   console.log('Request URL:'.brightCyan, req.originalUrl);
   console.log('Request Headers:'.brightCyan, req.headers);
+  console.log('Request Headers.origin:'.brightCyan, req.headers.origin);
   console.log('Request Body:'.brightCyan, req.body);
   console.log('Request Query:'.brightCyan, req.query);
   console.log('------------------------'.yellow);
