33074e4083a0b33f097647dd33bc0fc6c31db3e7
Jason Takahashi <jmtakahashi@gmail.com>
2026-03-16 23:28:20 -0700

    add https://todo-list-node-backend.onrender.com to allowed origins to test if cors error will be thrown.

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
-  "http://localhost:3000",
-  "https://todo-list-node-frontend.vercel.app",
-  "https://todo-list-node-frontend.onrender.com",
+  'http://localhost:3000',
+  'https://todo-list-node-frontend.vercel.app',
+  'https://todo-list-node-frontend.onrender.com',
+  'https://todo-list-node-backend.onrender.com',
 ];
 
 module.exports = allowedOrigins;
